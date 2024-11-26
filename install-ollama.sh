48937348355e9dce8d427041467c08b4cdb03547
Shisei Hanai <ruimo.uno@gmail.com>
2024-11-26 13:15:03 +0900

    install ollama

--- install-ollama.sh
+++ install-ollama.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-  echo "引数で十分の容量のあるデバイスのディレクトリ1つ指定してください(例: /mnt/vdd1/ollama)"
+  echo "引数で十分の容量のあるデバイスのディレクトリを指定してください(例: /mnt/vdd1/ollama)"
   exit 1
 fi
 
 export OLLAMA_MODELS="$1"
 curl -fsSL https://ollama.com/install.sh | sh
+
+line_to_add="Environment="OLLAMA_MODELS=$1/ollama-models"
+sudo sed -i "/^Environment=.*/a ${line_to_add}" /etc/systemd/system/ollama.service
+sudo mkdir -f $1
+sudo chown ollama:ollama $1
+sudo systemctl daemon-reload
+sudo systemctl start ollama
