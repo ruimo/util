109a298e9e7cad0b388685a9cd4e86d483bb5685
Shisei Hanai <ruimo.uno@gmail.com>
2024-11-26 13:02:09 +0900

    install ollama

--- install-ollama.sh
+++ install-ollama.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-curl -fsSL https://ollama.com/install.sh | sh
+
+if [ $# -ne 1 ]; then
+  echo "引数で十分の容量のあるデバイスのディレクトリ1つ指定してください(例: /mnt/vdd1/ollama)"
+  exit 1
+fi
+
+OLLAMA_MODELS="$1" curl -fsSL https://ollama.com/install.sh | sh
